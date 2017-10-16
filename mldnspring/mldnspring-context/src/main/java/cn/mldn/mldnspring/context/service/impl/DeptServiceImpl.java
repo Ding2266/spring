9b1465f9ea7bd58560d351da45be9272fa7d4402
Administrator <dj5827143@163.com>
2017-10-16 23:47:13 +0800

    springTest 程序测试

--- mldnspring/mldnspring-context/src/main/java/cn/mldn/mldnspring/context/service/impl/DeptServiceImpl.java
+++ mldnspring/mldnspring-context/src/main/java/cn/mldn/mldnspring/context/service/impl/DeptServiceImpl.java
@@ -11,10 +11,7 @@ import cn.mldn.mldnspring.context.vo.Dept;
 @Service
 public class DeptServiceImpl implements IDeptService {
 	@Resource	// <property name="deptDAO" ref="deptDAO"/>
-	private IDeptDAO deptDAO; // 定义IDeptDAO接口对象，表示该对象通过容器负责注入，会根据类型匹配
-	public void setDeptDAO(IDeptDAO deptDAO) {
-		this.deptDAO = deptDAO;
-	}
+	private IDeptDAO deptDAO;
 	@Override
 	public boolean add(Dept dept) {
 		System.out.println("******** Dept业务层调用 *********");
