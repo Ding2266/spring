989b83265143ed7930fb07f8144378ee6607ba35
Administrator <dj5827143@163.com>
2017-10-17 01:05:02 +0800

    Bean类配置

---
+++ mldnspring/mldnspring-context/src/main/java/cn/mldn/mldnspring/context/config/MLDNDAOConfig.java
@@ -0,0 +1,14 @@
+package cn.mldn.mldnspring.context.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cn.mldn.mldnspring.context.dao.IDeptDAO;
+import cn.mldn.mldnspring.context.dao.impl.DeptDAOImpl;
+@Configuration
+public class MLDNDAOConfig {
+	@Bean(name="deptDAOImpl")
+	public IDeptDAO getDeptDAOInstance() {
+		return new DeptDAOImpl() ; 
+	}
+}
