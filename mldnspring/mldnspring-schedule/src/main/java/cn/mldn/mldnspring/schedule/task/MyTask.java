bd912aeb4624c34ab774afcd7e43ae44fcfc120e
Administrator <dj5827143@163.com>
2017-10-18 00:23:38 +0800

    基于Annotation配置SpringTask

--- mldnspring/mldnspring-schedule/src/main/java/cn/mldn/mldnspring/schedule/task/MyTask.java
+++ mldnspring/mldnspring-schedule/src/main/java/cn/mldn/mldnspring/schedule/task/MyTask.java
@@ -2,10 +2,14 @@ package cn.mldn.mldnspring.schedule.task;
 
 import java.text.SimpleDateFormat;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 
 
+@Component  //必须明确的告诉Spring，现在的MyTask是一个Spring容器管理的bean
 public class MyTask{
+	@Scheduled(cron="* * * * * ?")
 	public void runJobTask(){
 		System.out.println("当前的日期时间" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
 	}
