b1dc56fd8420a471c80ecdb019706207c265d3df
Administrator <dj5827143@163.com>
2017-10-18 00:34:48 +0800

    任务调度池

--- mldnspring/mldnspring-schedule/src/main/java/cn/mldn/mldnspring/schedule/task/MyTaskFirst.java
+++ mldnspring/mldnspring-schedule/src/main/java/cn/mldn/mldnspring/schedule/task/MyTaskFirst.java
@@ -1,6 +1,7 @@
 package cn.mldn.mldnspring.schedule.task;
 
 import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,10 @@ import org.springframework.stereotype.Component;
 
 
 @Component  //必须明确的告诉Spring，现在的MyTask是一个Spring容器管理的bean
-public class MyTask{
+public class MyTaskFirst{
 	@Scheduled(cron="* * * * * ?")
-	public void runJobTask(){
-		System.out.println("当前的日期时间" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
+	public void runJobTask() throws InterruptedException{
+		TimeUnit.SECONDS.sleep(5);
+		System.out.println("@@@@【第一个任务】-当前的日期时间" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
 	}
 }
