abcec2cdd93e662cbd339a6b7d94da5c17d7a940
Administrator <dj5827143@163.com>
2017-10-22 20:36:32 +0800

    SpringMVC拦截器（HandlerMethod类对象）

--- mldnspring/mldnspring-interceptor/src/main/java/cn/mldn/util/ValidationInterceptor.java
+++ mldnspring/mldnspring-interceptor/src/main/java/cn/mldn/util/ValidationInterceptor.java
@@ -5,8 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 //由于所有的抽象方法都是用了default关键字，那么此时不会再默认实现若干个抽象方法
 public class ValidationInterceptor implements HandlerInterceptor {	
@@ -15,20 +15,18 @@ public class ValidationInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		this.logger.info("@@@1、【**** preHandle ****】" + handler.getClass());
+		if(handler instanceof HandlerMethod) {
+			HandlerMethod handlerMethod = (HandlerMethod)handler ;
+			this.logger.info("【处理的Action】对象" + handlerMethod.getBean()) ; 
+			this.logger.info("【处理Action—Class】" + handlerMethod.getBeanType());
+			this.logger.info("【Action处理方法名称】" + handlerMethod.getMethod());
+			for(int x=0 ; x<handlerMethod.getMethodParameters().length ; x++) {
+				this.logger.info(
+						"@@【Action处理方法参数-" + x + "】" + handlerMethod.getMethodParameters()[x].getParameterName());
+			}
+		}
 		return true ; //返回true表示放行，而如果返回了false表示不执行后续的Action或拦截器
 	}
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		this.logger.info("@@@2、【**** postHandle ****】" + handler.getClass());
-		
-	}
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		this.logger.info("@@@3、【**** afterCompletion ****】" + handler.getClass());
-
-	}
+	
 	
 }
